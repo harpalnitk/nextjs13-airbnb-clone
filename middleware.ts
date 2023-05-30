87bd0a6a848ab83fb8e039e0dd1b5c876ac41ca2
Harpal Singh <harpalnitk@gmail.com>
2023-05-30 12:02:05 +0530

    added message

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,9 @@ export { default } from "next-auth/middleware"
 
 
 //to protect routes using next-auth
+// so that we cannot manually write the URL
+//which is protected and go there
+//when not signed in
 export const config = { 
   matcher: [
     "/trips",
